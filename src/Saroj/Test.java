89603e921d38c6c63c501e42461554229002458f
saroj kaul <sarojmahi73@gmail.com>
2020-10-29 10:47:03 +0100

    adding saroj main file

--- src/Saroj/Test.java
+++ src/Saroj/Test.java
@@ -1,4 +1,22 @@
 package Saroj;
 
 public class Test {
-}
+    public static void main(String[] args) {
+        double number = 10.0;
+        if (number == 0) {
+            System.out.println("number is zero");
+        } else if (number < 0) {
+            System.out.println("number is negative");
+        } else if (number > 0) {
+            System.out.println("number is positive");
+        }
+        if (number<1){
+            System.out.println("number is small");
+        }
+        else if  (number > 1,000, 000){
+            System.out.println("number is large");
+        }
+    }
+
+
+    }
