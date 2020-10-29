b29e6332ed429fd3edea3de86c6d38eadd80b33b
saroj kaul <sarojmahi73@gmail.com>
2020-10-29 11:10:03 +0100

    adding test file

--- src/Saroj/Test.java
+++ src/Saroj/Test.java
@@ -2,7 +2,7 @@ package Saroj;
 
 public class Test {
     public static void main(String[] args) {
-        double number = 10.0;
+        /*double number = 10.0;
         if (number == 0) {
             System.out.println("number is zero");
         } else if (number < 0) {
@@ -15,8 +15,32 @@ public class Test {
         }
         else if  (number > 1000000){
             System.out.println("number is large");
+        }*/
+
+        int x = 2;
+        switch (x) {
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Thursday");
+                break;
+            case 4:
+                System.out.print("Wednesday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("saturday");
+                break;
+            default:
+                System.out.print("sunday");
+
         }
+
     }
-
-
     }
